ffbd2138ff6667b03b016740cee4459c433cd9d8
Tobias Leonhard Joschka Peslalz <peslalz.tobias@hm.edu>
2021-04-25 20:27:34 +0200

    :tada: Done :)

--- app/src/main/java/edu/hm/cs/w5/MainActivity.kt
+++ app/src/main/java/edu/hm/cs/w5/MainActivity.kt
@@ -1,10 +1,11 @@
 package edu.hm.cs.w5
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.GridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +20,11 @@ class MainActivity : AppCompatActivity() {
         images.apiKey = getString(R.string.nasa_api_key)
 
         recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        if (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            recyclerView.layoutManager = GridLayoutManager(this,2)
+        }else{
+            recyclerView.layoutManager = GridLayoutManager(this,3)
+        }
 
         images.imagesLiveData.observe(this, Observer {
             adapter.images = it
